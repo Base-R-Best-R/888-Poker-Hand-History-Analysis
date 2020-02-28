508a5aadeed2d0f1393b23d0a4e0452a04ac724f
StackOverthink <56105976+StackOverthink@users.noreply.github.com>
2020-02-28 15:17:52 +0100

    Update Stats.R

--- Stats.R
+++ Stats.R
@@ -3,5 +3,15 @@ getwd()
 setwd("C:/Users/blasc/OneDrive/Documents/GitHub/888-Poker-Hand-History-Analysis/R Data") # can be found in the R Data folder
 Hands <- readRDS(list.files()[2])
 # VPIP - measures how often one voluntarily pays money into a pot before seeing the flop
-# screen info
+f <- lapply(Hands, function(x){
+  lapply(x, grep, pattern = "B2B_Suckouts bets|B2B_Suckouts raises")
+})
+#
+m <- lapply(f, function(x){
+  lapply(x, function(y){
+    2 %in% y
+  })
+})
+# VPIP
+VPIP <- sum(unlist(m)) / sum(sapply(Hands, length)) # Interestingly quite a bit tighter than expected
 
