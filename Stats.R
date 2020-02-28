4a0508ae4fd7535da10ab5eec660793dbf9fd53a
StackOverthink <56105976+StackOverthink@users.noreply.github.com>
2020-02-28 14:45:14 +0100

    Create Stats.R

---
+++ Stats.R
@@ -0,0 +1,7 @@
+## Statistics ##
+getwd()
+setwd("C:/Users/blasc/OneDrive/Documents/GitHub/888-Poker-Hand-History-Analysis/R Data") # can be found in the R Data folder
+Hands <- readRDS(list.files()[2])
+# VPIP - measures how often one voluntarily pays money into a pot before seeing the flop
+# screen info
+
